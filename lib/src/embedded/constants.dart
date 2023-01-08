542931c108d73a6cf177c36eec2519932dc27f11
Sol Sanctum <so1_sanctum@pm.me>
2023-01-08 18:17:00 -0500

    updated htlc param

--- lib/src/embedded/constants.dart
+++ lib/src/embedded/constants.dart
@@ -56,7 +56,7 @@ const int sporkDescriptionMaxLength = 400;
 
 // Htlc
 const int htlcPreimageMinLength = 32;
-const int htlcPreimageMaxLength = 32;
+const int htlcPreimageMaxLength = 255;
 const int htlcTimelockUnitSec = 60 * 60; // 1 hour
 const int htlcTimelockMinSec = htlcTimelockUnitSec * 1; // 1 hour
 const int htlcTimelockMaxSec = htlcTimelockUnitSec * 24 * 30 * 12; // ~1 year
